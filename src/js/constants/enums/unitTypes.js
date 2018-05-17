14a3e8ac420e01ec3055b88abf781a889ab0e46b
JaredLeeJardine <jared@jardinefam.com>
2018-05-16 20:51:03 -0600

    Unit types

---
+++ src/js/constants/enums/unitTypes.js
@@ -0,0 +1,12 @@
+export const UnitTypes = Object.freeze({
+  ARCHER: 'archer',
+  BOWMAN: 'bowman',
+  BUILDER: 'builder',
+  CHAMPION: 'champion',
+  KNIGHT: 'knight',
+  PEASANT: 'peasant',
+  PEASANT_BOWMAN: 'peasant_bowman',
+  RANGER: 'ranger',
+  SOLDIER: 'soldier',
+  SQUIRE: 'squire'
+});
